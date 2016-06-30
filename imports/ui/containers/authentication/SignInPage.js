a50ed8de4d610706b266f00678e2a8a080a5e89d
Tan Khuu <jackiekhuu.work@google.com>
2016-06-30 10:37:35 +0700

    in sign in alias, logout user after they change to subdomain (for making sure the consistency of user login & subdomain)

--- imports/ui/containers/authentication/SignInPage.js
+++ imports/ui/containers/authentication/SignInPage.js
@@ -44,8 +44,6 @@ export default class SignInPage extends Component {
           if(subdomain === userAlias) {
             FlowRouter.go(userHomeRoute.path);
           } else {
-            // for user login in a consistency state
-            Meteor.logout();
             this.setState({
               alias: false,
               errors: `${email} doesn't belong to ${document.location.hostname}. Please enter the correct alias.`
@@ -66,6 +64,8 @@ export default class SignInPage extends Component {
       }
     };
     SubdomainActions.addSubdomain(context);
+    // for user login in a consistency state
+    Meteor.logout();
   }
 
   render() {
