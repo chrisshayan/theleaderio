a7bf2d2fedb3acf287bf4e5f66156831161664a5
Phu Nguyen <mrphu3074@phus-MacBook-Pro.local>
2016-07-15 12:02:42 +0700

    fix edit profile url

--- imports/ui/common/TopNav.js
+++ imports/ui/common/TopNav.js
@@ -37,7 +37,7 @@ class TopNav extends Component {
                   <span className="caret"></span>
                 </a>
                 <ul className="dropdown-menu" aria-labelledby="user-info">
-                  <li><a href={FlowRouter.url('app.profile.edit')}>Edit profile</a></li>
+                  <li><a href={FlowRouter.url('app.profile.edit', {action: 'edit'})}>Edit profile</a></li>
                   <li role="separator" className="divider"></li>
                   <li><a href={FlowRouter.url('app.logout')}>Logout</a></li>
                 </ul>
