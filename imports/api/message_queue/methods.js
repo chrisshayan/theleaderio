8fec517c8c2bec4aaf53598e73a508797e77c164
Tan Khuu <jackiekhuu.work@google.com>
2016-08-15 00:52:56 +0700

    solution for enqueueing sending metric email message on different timezones

--- imports/api/message_queue/methods.js
+++ imports/api/message_queue/methods.js
@@ -8,6 +8,17 @@ import {QueueCollection} from '/imports/api/message_queue/index';
  *  Enqueue methods
  *  params: date, data
  */
+// solution for enqueueing sending metric email message on different timezones
+// enqueue with job-collection:
+//  const sendDate = getLocalDate(date, timezone);
+//  const job = new Job(queue);
+//  job.after(sendDate).save();
+//
+// enqueue with rabbitMQ:
+//  const jobRunDate = "2016-08-15 09:00" // in local timezone (the running time of job)
+//  const sendDate = getLocalDate(date, timezone); // get send date in local timezone
+//  const delayTime = jobRunDate.diff(sendDate);
+//  enqueue into rabbitMQ with x-delay(delayTime)
 
 export const enqueue = new ValidatedMethod({
   name: 'mq.enqueue',
--- imports/api/time/functions.js
+++ imports/api/time/functions.js
@@ -1,6 +1,13 @@
+import {Meteor} from 'meteor/meteor';
 
+/**
+ * Get local datetime
+ * @param date
+ * @param timezone
+ * @returns a datetime for local timezone
+ */
 export const getLocalDate = (date, timezone) => {
-  const localTimezone = 'Asia/Ho_Chi_Minh';
+  const localTimezone = Meteor.settings.public.localTimezone;
   const timezoneDate = moment.tz(date, timezone);
   const localDate = timezoneDate.clone().tz(localTimezone);
   return localDate.format();
